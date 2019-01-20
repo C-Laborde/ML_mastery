55f83e41cb034ccef902f71bdff2482cbc34dc15
cecilia_laborde <cecilialaborde@gmail.com>
2019-01-20 15:05:51 +0100

    autoregression model

--- L5_autoregressive_forecast.py
+++ L5_autoregressive_forecast.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from pandas import Series
 import matplotlib.pyplot as plt
 from pandas.plotting import autocorrelation_plot
 from pandas.plotting import lag_plot
 from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.ar_model import AR
 from utils.persistence_model import persistence_model
 
 
@@ -46,7 +48,7 @@ train, test = X[1:len(X)-7], X[len(X)-7:]
 X_train, y_train = train[:, 0], train[:, 1]
 X_test, y_test = test[:, 0], test[:, 1]
 
-# walk-forward validation
+# walk-forward validation for 7 1-day forecasts
 predictions = list()
 for x in X_test:
     yhat = persistence_model(x)
@@ -61,6 +63,26 @@ ax.plot(predictions, color="green")
 ax.legend()
 
 
-# model = AR()
+# #####  Autoregression model for 1 7-days forecast ######
+# train Autoregression
+series = Series.from_csv('data/daily-minimum-temperatures.csv', header=0)
+X = series.values
+train, test = X[1:len(X)-7], X[len(X)-7:]
+model = AR(train)           # this creates the model
+model_fit = model.fit()     # this trains the model in the dataset
+print("Lag length: %s" % model_fit.k_ar)
+print("Coefficients %s" % model_fit.params)   # The fitted parameters
 
+# make predictions
+predictions = model_fit.predict(start=len(train), end=len(train)+len(test)-1,
+                                dynamic=False)
+for i in range(len(predictions)):
+    print("predicted=%f, expected=%f" % (predictions[i], test[i]))
+
+error = mean_squared_error(test, predictions)
+print("Test MSE: %f" % error)
+
+fix, ax = plt.subplots(1, 1)
+ax.plot(test)
+ax.plot(predictions, color="green")
 plt.show()
