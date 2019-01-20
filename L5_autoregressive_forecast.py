6f0d46d7ecc5c9898b97d0f04b0543466d084b7d
cecilia_laborde <cecilialaborde@gmail.com>
2019-01-20 14:05:05 +0100

    autocorrelation matrix

--- L5_autoregressive_forecast.py
+++ L5_autoregressive_forecast.py
@@ -13,17 +13,24 @@ series = pd.read_csv('data/daily-minimum-temperatures.csv', header=0,
                      index_col=0, parse_dates=True)
 
 """
-Autoregression: Is a time series model that uses observations from previous
-time steps as input to a regression equation to predict the value at the next
-time step.
+Autoregression: Is a linear time series model that uses observations from
+previous time steps as input to a regression equation to predict the value at
+the next time step.
 """
 print(series.tail())
 series.column = ['Temp']
 
+# Plot time series
 plt.figure()
 plt.plot(series)
 plt.ylabel("Temperature")
-plt.xlabel("Year")
+plt.xlabel("Date")
+
+# Calculate correlation matrix
+df = pd.concat([series.shift(1), series], axis=1)
+df.columns = ["t-1", "t+1"]
+result = df.corr()
+print(result)
 
 # Check autocorrelation
 fig, ((ax1, ax2)) = plt.subplots(2, 1)
@@ -31,7 +38,9 @@ autocorrelation_plot(series, ax=ax1)
 diag = range(int(series.min()), int(series.max()))
 lag_plot(series, ax=ax2)
 plt.plot(diag, diag, '--k')
-
 fig.subplots_adjust(hspace=0.4)
+
+
+# model = AR()
 
 plt.show()
