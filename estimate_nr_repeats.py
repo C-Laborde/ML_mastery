ce03df49b2dc04a0b91dacf62ed4a6a1650ddb98
cecilia_laborde <cecilialaborde@gmail.com>
2019-02-13 17:23:00 +0100

    implemented estimation of repetitions required

--- estimate_nr_repeats.py
+++ estimate_nr_repeats.py
@@ -24,11 +24,25 @@ fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.boxplot(df.iloc[:, 0])
 ax2.hist(df["results"])
 
-print(df.iloc[:5].mean())
 # check if the average stabilizes after 1000 results
 partial_avg = [(df.iloc[:i]).mean() for i in range(df.size)]
 fig, ax = plt.subplots(1, 1)
 ax.plot(partial_avg)
+ax.axhline(y=df["results"].mean(), color="r")
 
+# standard_error = sample_standard_deviation / sqrt(number of repeats)
+partial_ste = [df.iloc[:i].std()/i**.5 for i in range(1, df.size+1)]
+fig, ax = plt.subplots(1, 1)
+ax.plot(partial_ste)
+# the horizontal lines help to determine what number of experiments are
+# required for different standar deviations
+ax.axhline(y=0.5, color="r")
+ax.axhline(y=1, color="r")
 
+# we calculate the confidence interval for each number of repeats and add it
+# as error bars
+fig, ax = plt.subplots(1, 1)
+conf = [std * 1.96 for std in partial_ste]
+plt.errorbar(range(len(partial_avg)), partial_avg, yerr=conf)
+ax.axhline(y=df["results"].mean(), color="r")
 plt.show()
